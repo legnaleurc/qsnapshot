ee827eac07d198c843fd413c4f326bc9516e7732
Wei Cheng Pan <legnaleurc@gmail.com>
2012-05-07 05:24:11 +0800

    Add translation file.

---
+++ locales/zh_TW.ts
@@ -0,0 +1,95 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="zh_TW">
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="../src/widget/qsnapshot.cpp" line="39"/>
+        <source> second(s)</source>
+        <translation> 秒</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.cpp" line="40"/>
+        <source>Preview of the snapshot image (%1 x %2)</source>
+        <translation>擷圖預覽 (%1 x %2)</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/regiongrabber.cpp" line="54"/>
+        <source>Select a region using the mouse. To take the snapshot, press the Enter key or double click. Press Esc to quit.</source>
+        <translation>使用滑鼠選擇一個區域。雙擊左鍵或按下 Enter 鍵擷圖。按下 Esc 離開。</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/snapshottimer.cpp" line="28"/>
+        <source>Snapshot will be taken in %1 second(s)</source>
+        <translation>將在 %1 秒後擷圖</translation>
+    </message>
+</context>
+<context>
+    <name>QSnapshot</name>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="14"/>
+        <source>QSnapshot</source>
+        <translation>QSnapshot</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="51"/>
+        <source>Take a New Snapshot</source>
+        <translation>擷圖</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="82"/>
+        <source>Capture mode</source>
+        <translation>擷圖模式</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="93"/>
+        <source>Full Screen</source>
+        <translation>全螢幕</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="98"/>
+        <source>Window Under Cursor</source>
+        <translation>游標下的視窗</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="103"/>
+        <source>Rectangular Region</source>
+        <translation>方形區域</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="108"/>
+        <source>Section of Window</source>
+        <translation>視窗部件</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="116"/>
+        <source>Snapshot delay</source>
+        <translation>擷圖延遲</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="129"/>
+        <source>Include window decorations</source>
+        <translation>包含視窗邊框</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="149"/>
+        <source>Include mouse pointer</source>
+        <translation>包含滑鼠游標</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="177"/>
+        <source>Help</source>
+        <translation>幫助</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="197"/>
+        <source>Copy to Clipboard</source>
+        <translation>複製到剪貼簿</translation>
+    </message>
+    <message>
+        <location filename="../src/widget/qsnapshot.ui" line="204"/>
+        <source>Save As ...</source>
+        <translation>另存新檔 ...</translation>
+    </message>
+</context>
+</TS>
