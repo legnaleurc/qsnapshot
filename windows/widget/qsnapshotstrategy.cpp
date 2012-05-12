b0c8bce4c0aaa021b9ad77e5256c7564cbd93c2c
Wei Cheng Pan <legnaleurc@gmail.com>
2012-05-12 19:05:46 +0800

    Fix compile error.

--- windows/widget/qsnapshotstrategy.cpp
+++ windows/widget/qsnapshotstrategy.cpp
@@ -1,35 +1,58 @@
+/*
+    QSnapshot, a screen capture tool.
+    Copyright (C)  2012 Wei Cheng Pan <legnaleurc@gmail>
+
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
 #include "qsnapshotstrategy.hpp"
 
 # include <QtCore/QLibrary>
 
 using qsnapshot::widget::QSnapshot;
+using qsnapshot::widget::QSnapshotStrategy;
 
-const bool QSnapshot::Strategy::HACK = inject();
+namespace {
 
-bool QSnapshot::Strategy::inject() {
-	QSnapshot::Private::creator() = []( QSnapshot * host )->QSnapshot::Private * {
-		return new QSnapshot::Strategy( host );
-	};
-	return true;
+	bool inject() {
+		QSnapshot::Strategy::creator() = []( QSnapshot * host )->QSnapshot::Strategy * {
+			return new QSnapshotStrategy( host );
+		};
+		return true;
+	}
+
+	const bool HACK = inject();
+
 }
 
-QSnapshot::Strategy::Strategy( QSnapshot * host ):
-QSnapshot::Private( host ),
+QSnapshotStrategy::QSnapshotStrategy( QSnapshot * host ):
+QSnapshot::Strategy( host ),
 // load dll at run-time because Windows XP does not support DWM
 f( reinterpret_cast< Function >( QLibrary::resolve( "dwmapi", "DwmSetWindowAttribute" ) ) ) {
 }
 
-void QSnapshot::Strategy::fastHide() {
+void QSnapshotStrategy::fastHide() {
 	this->disableDWM( true );
 	this->host->hide();
 }
 
-void QSnapshot::Strategy::fastShow() {
+void QSnapshotStrategy::fastShow() {
 	this->disableDWM( false );
 	this->host->show();
 }
 
-void QSnapshot::Strategy::disableDWM( bool disable ) {
+void QSnapshotStrategy::disableDWM( bool disable ) {
 	if( this->f == nullptr ) {
 		return;
 	}
--- windows/widget/qsnapshotstrategy.hpp
+++ windows/widget/qsnapshotstrategy.hpp
@@ -1,21 +1,36 @@
-#ifndef QSNAPSHOT_QSNAPSHOTSTRATEGY_HPP
-#define QSNAPSHOT_QSNAPSHOTSTRATEGY_HPP
+/*
+    QSnapshot, a screen capture tool.
+    Copyright (C)  2012 Wei Cheng Pan <legnaleurc@gmail>
 
-#include "../../src/widget/qsnapshotprivate.hpp"
+    This library is free software; you can redistribute it and/or
+    modify it under the terms of the GNU Lesser General Public
+    License as published by the Free Software Foundation; either
+    version 2.1 of the License, or (at your option) any later version.
+
+    This library is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+    Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public
+    License along with this library; if not, write to the Free Software
+    Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+#ifndef QSNAPSHOT_WIDGET_QSNAPSHOTSTRATEGY_HPP
+#define QSNAPSHOT_WIDGET_QSNAPSHOTSTRATEGY_HPP
+
+#include "widget/qsnapshotstrategy.hpp"
 
 #include <dwmapi.h>
 
 namespace qsnapshot {
 	namespace widget {
 
-		class QSnapshot::Strategy : public QSnapshot::Private {
+		class QSnapshotStrategy : public QSnapshot::Strategy {
 		public:
 			typedef HRESULT ( WINAPI * Function )( HWND, DWORD, LPCVOID, DWORD );
 
-			static const bool HACK;
-			static bool inject();
-
-			explicit Strategy( QSnapshot * host );
+			explicit QSnapshotStrategy( QSnapshot * host );
 
 			virtual void fastHide();
 			virtual void fastShow();
